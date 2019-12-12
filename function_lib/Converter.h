2a29726a0cebb7a47067470ac0a82cc6814a5cb5
Pavel3412 <Pavel3412>
2019-12-12 22:11:17 +0300

    qq

---
+++ function_lib/Converter.h
@@ -0,0 +1,32 @@
+﻿#ifndef __POSTFIX_H__
+#define __POSTFIX_H__
+
+#include <string>
+#include "stack.h"
+#include <iostream>
+#include <sstream>
+#include <fstream>
+
+using namespace std;
+
+string StringDecoder(string);
+
+class TPostfix
+{
+	string infix;
+	string postfix;
+public:
+	TPostfix()
+	{
+		infix = "a + b";
+	}
+	string GetInfix() { return infix; }
+	string GetPostfix() { return postfix; }
+	void SetInfix(string expression) { infix = StringDecoder(expression); }
+	string ToPostfix();
+	double Calculate(string a = ""); // Ввод переменных, вычисление по постфиксной форме
+};
+
+
+
+#endif
---
+++ function_lib/Stack.h
@@ -0,0 +1,67 @@
+﻿#ifndef __STACK_H__
+#define __STACK_H__
+
+const int MaxStackSize = 100;
+
+template <class T>
+class TStack
+{
+	T* pMem;
+	int size;
+	int top;
+public:
+	TStack(int _size = MaxStackSize)
+	{
+		size = _size;
+		top = -1;
+		if ((size < 1) || (size > MaxStackSize))
+			throw size;
+		pMem = new T[size];
+	}
+
+	void print()
+	{
+		for (int i = 0; i <= top; ++i)
+			cout << pMem[i] << " ";
+		cout << endl;
+	}
+
+	bool isEmpty()
+	{
+		if (top < 0)
+			return true;
+		else
+			return false;
+
+	}
+	T pop()
+	{
+		if (!isEmpty())
+			return pMem[top--];
+		else
+			throw 1;
+	}
+
+	T Top()
+	{
+		if (!isEmpty())
+			return pMem[top];
+		else
+			throw 1;
+	}
+
+	void push(T val)
+	{
+		if (top + 1 < size)
+			pMem[++top] = val;
+		else
+			throw 1;
+	}
+
+	~TStack()
+	{
+		delete[] pMem;
+	}
+};
+
+#endif
